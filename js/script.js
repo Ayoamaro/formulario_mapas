0d7ff35c80a197fe435107d0d8d5332cdddd0694
JesusOran <58339252+JesusOran@users.noreply.github.com>
2020-01-11 20:27:48 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 var marcador;
 var markerGroupFiltro;
-var mymap = L.map("mymap").setView([28.0491390846109, -16.6629893552163], 13);
+var mymap = L.map("mymap").setView([28.4895723, -16.31815646], 13);
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 80,
+    maxZoom: 100,
     id: "mapbox/streets-v11",
     accessToken:
       "pk.eyJ1IjoiamVzdXNvcmFuIiwiYSI6ImNrNGNxYndxejBkdG4zbG0wN2VkeXhwam0ifQ.PQYLiFFdeZtUIXlekRLpjg"
   }
 ).addTo(mymap);
+mymap.setZoom(18);
 
 const datos = getDatos();
 var boton = document.getElementById("boton");
@@ -22,7 +23,9 @@ for (var i = 0; i < datos[0].features.length; i++) {
   L.marker([
     datos[0].features[i].geometry.coordinates[1],
     datos[0].features[i].geometry.coordinates[0]
-  ]).addTo(markerGroup);
+  ])
+    .bindPopup("<b>" + datos[0].features[i].properties.nombre)
+    .addTo(markerGroup);
 }
 mymap.addLayer(markerGroup);
 
@@ -38,7 +41,8 @@ function filtroBusqueda() {
         if (datos[0].features[i].properties.nombre.includes(valor)) {
           arrayCoord.push({
             coord1: datos[0].features[i].geometry.coordinates[1],
-            coord2: datos[0].features[i].geometry.coordinates[0]
+            coord2: datos[0].features[i].geometry.coordinates[0],
+            nombre: datos[0].features[i].properties.nombre
           });
         }
       }
@@ -48,21 +52,22 @@ function filtroBusqueda() {
         if (datos[0].features[i].properties.cp === parseInt(valor)) {
           arrayCoord.push({
             coord1: datos[0].features[i].geometry.coordinates[1],
-            coord2: datos[0].features[i].geometry.coordinates[0]
+            coord2: datos[0].features[i].geometry.coordinates[0],
+            nombre: datos[0].features[i].properties.nombre
           });
         }
       }
       break;
   }
-  
   if (markerGroupFiltro != undefined) {
     mymap.removeLayer(markerGroupFiltro);
   }
   markerGroupFiltro = L.layerGroup().addTo(mymap);
   for (var i = 0; i < arrayCoord.length; i++) {
-    L.marker([arrayCoord[i].coord1, arrayCoord[i].coord2]).addTo(
-      markerGroupFiltro
-    );
+    L.marker([arrayCoord[i].coord1, arrayCoord[i].coord2])
+      .bindPopup("<b>" + arrayCoord[i].nombre)
+      .addTo(markerGroupFiltro);
   }
   mymap.addLayer(markerGroupFiltro);
 }
+
