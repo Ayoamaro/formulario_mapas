f1471e7827c6a84e49924188ac67ffe8ed481e4d
Ayoamaro <48093528+Ayoamaro@users.noreply.github.com>
2020-01-12 18:05:05 +0000

    Update init.js

--- js/init.js
+++ js/init.js
@@ -83,20 +83,3 @@ function inicioSesionGoogle() {
     console.log(credential);
   });
 }
-
-var FBprovider = new firebase.auth.FacebookAuthProvider();
-
-function inicioSesionFacebook() {
-  firebase.auth().signInWithPopup(FBprovider).then(function(result) {
-    var token = result.credential.accessToken;
-    var user = result.user;
-
-  }).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    console.log(email);
-    console.log(credential);
-  });
-}
